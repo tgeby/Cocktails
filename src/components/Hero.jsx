ae834cf15ddbcd9c15069aa9841f83d639adec36
Thomas Eby <tgeby@icloud.com>
2025-12-24 08:38:13 -0500

    Added #t=0.001 to video src to try to get the video to show on mobile.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -120,7 +120,7 @@ const Hero = () => {
             <div className="video absolute inset-0">
                 <video 
                     ref={videoRef}
-                    src="videos/output.mp4"
+                    src="videos/output.mp4#t=0.001"
                     muted
                     playsInline
                     preload="auto"
